6b5a1ed85dc30082507aab648d879793a0bf6fdc
18214290016 <18214290016@OLAB04-D16.academico.iesb.br>
2019-08-15 21:19:07 -0300

    aula 02, dia 15/08/2019

--- OlaMundo/src/Atividade01/TestaFuncionario.java
+++ OlaMundo/src/Atividade01/TestaFuncionario.java
@@ -1,4 +1,4 @@
-
+//Classe aplicação que testa a classe Funcionario
 package Atividade01;
 
     import Atividade01.Funcionario;
@@ -6,7 +6,8 @@ package Atividade01;
 public class TestaFuncionario {
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario(182145,"Natanzin",Pessoa.SEXO_MASCULINO,10500);
-        System.out.println(funcionario.getMatricula() + "-" + funcionario.getNome() + " - " + funcionario.getSexo() + " - " + funcionario.getSalario());
+        //System.out.println(funcionario.getMatricula() + "-" + funcionario.getNome() + " - " + funcionario.getSexo() + " - " + funcionario.getSalario());
+        System.out.println(funcionario);
     }
     
     
--- OlaMundo/src/Atividade01/TestaPessoa.java
+++ OlaMundo/src/Atividade01/TestaPessoa.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Classe aplicação que testa a classe Pessoa
 package Atividade01;
 
     import Atividade01.Pessoa;
