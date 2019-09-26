b566f57caae53940da20b05e47cd8f5e3292954a
Natanzin <natan.silva.judo@gmail.com>
2019-09-26 16:28:37 -0300

    FINAL COMMIT 1°BIM

---
+++ Atividade07/src/atividade07/e.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade07;
+
+/**
+ *
+ * @author NatanSilva
+ */
+class e {
+
+    static char getKeyChar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void setKeyChar(char toUpperCase) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
