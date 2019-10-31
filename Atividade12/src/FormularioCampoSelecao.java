85bec306f1ec2b8f6238c7614072a93cf515b158
Natanzin <natan.silva.judo@gmail.com>
2019-10-31 21:23:02 -0200

    Atividade12

---
+++ Atividade12/src/FormularioCampoSelecao.java
@@ -0,0 +1,48 @@
+
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.ListSelectionModel;
+
+
+public class FormularioCampoSelecao extends JFrame{
+    JComboBox comboUF = new JComboBox();
+    JList listaMunicipio = new JList();
+    String[] arrayUF = {};
+    String[][] arrayMunicipio = {};
+
+    public FormularioCampoSelecao() throws HeadlessException {
+        arrayUF = new String[]{"DF","GO"};
+        arrayMunicipio = new String[][]{{"Brasília", "Taguatinga", "Sobradinho"},{"Formosa", "Planaltina", "Valparaiso"}};
+        comboUF = new JComboBox(arrayUF);
+        listaMunicipio = new JList(arrayMunicipio[0]);
+        listaMunicipio.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        comboUF.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                listaMunicipio.setListData(arrayMunicipio[comboUF.getSelectedIndex()]);
+            }
+        });
+        JLabel rotuloUF = new JLabel("Seleciona a UF:");
+        JLabel rotuloMunicipio = new JLabel("Seleciona o município:");
+        
+        JPanel painel = new JPanel();
+        painel.add(rotuloUF);
+        painel.add(comboUF);
+        painel.add(rotuloMunicipio);
+        painel.add(listaMunicipio);
+        add(painel);
+        
+        setSize(640, 480);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        
+    }
+    
+    
+}
---
+++ Atividade12/src/TestaFormularioCampoSelecao.java
@@ -0,0 +1,6 @@
+
+public class TestaFormularioCampoSelecao {
+    public static void main(String[] args) {
+        FormularioCampoSelecao formulario = new FormularioCampoSelecao();
+    }
+}
